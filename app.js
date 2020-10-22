f1d16950e8f214152f0d2f1214dd156a938f7de5
Victor Moscone <victormoscone@gmail.com>
2020-10-22 00:12:21 -0700

    Feat: Began working on the question sequence.

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 let startBtn = document.querySelector("#startBtn");
+let cardText = document.querySelector("#cardText");
+let titleText = document.querySelector("#titleText");
 
 startBtn.addEventListener("click", quizStart)
     // How we'll begin our quiz off of the Start button.
 
 function quizStart() {
-    console.log("Hello")
+    titleText.textContent = "Question 1"
+    cardText.textContent = "Do you got this?";
 }
+
+// Notes to self, and don't forget to delete these this time:
+    // I'm thinking each question is an array or object.
+    // Each arr/obj has four buttons that operate as your multiple choice and they each trigger a function
+    // to move onto the next question while also returning your answer as right or wrong.
