5d1e86922e8e4559cffae14a8125fbfff74c9eae
Victor Moscone <victormoscone@gmail.com>
2020-10-23 23:41:59 -0700

    Feat: Creating function to dynamically construct a question and its buttons.

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
 let startBtn = document.querySelector("#startBtn");
 let cardText = document.querySelector("#cardText");
 let titleText = document.querySelector("#titleText");
+let a1btn = document.createElement("BUTTON");
+let a2btn = document.createElement("BUTTON");
+let cardBody = document.querySelector("#cardBody");
+
+
+let questionOne = {
+    q1statement: "When would you use an Array instead of an Object?",
+    q1answer: "When wanting to have the properties in order.",
+}
 
 startBtn.addEventListener("click", quizStart)
     // How we'll begin our quiz off of the Start button.
 
+function resetQuestion() {
+    // Anything we need to dumb in here to reset each question.
+    startBtn.style.display = "none";
+    // This hides the start button shown on initial page load and quiz completion.
+}
+
+function createQButton(objectName) {
+    let btnDiv = document.createElement("div");
+    btnDiv.setAttribute("id", "questionButton");
+    cardBody.appendChild(btnDiv);
+    let test = document.querySelector("#questionButton");
+    a1btn.innerHTML = "Click me";
+    test.appendChild(a1btn);
+}
+
 function quizStart() {
-    titleText.textContent = "Question 1"
-    cardText.textContent = "Do you got this?";
+    resetQuestion();
+    titleText.textContent = questionOne.q1statement
+    cardText.textContent = questionOne.q1answer;
+    createQButton(questionOne);
+    // a1btn.innerHTML = "Click me";
+    // a2btn.innerHTML = "Click me";
+    // cardText.appendChild(a1btn);
+    // cardText.appendChild(a2btn)
 }
 
 // Notes to self, and don't forget to delete these this time:
